c5a284f25b100dc52477c92a9e0dd334122be44d
LeonardoJr96 <leo70x7000@gmail.com>
2025-07-27 17:43:30 -0300

    docs: :sparkles: add new information

---
+++ get-started/src/FirstCode.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FirstCode {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //Variavel scanner do tipo Scanner declarada
+        //var scanner = new Scanner(System.in); // Variavel scanner do tipo Scanner declarada com var
+        // "var" é uma palavra-chave do Java que permite declarar variáveis sem especificar
+        System.out.println("Olá, informe o seu nome:"); // Mensagem para o usuário
+        String name = scanner.next(); // String para armazenar o nome e não é uma classe primitiva
+        // var name = scanner.next(); // String para armazenar o nome com var
+        System.out.println("informe sua idade:"); // Mensagem para o usuário
+        int age = scanner.nextInt(); // int para armazenar a idade, que é uma classe primitiva
+        // var age = scanner.nextInt(); // int para armazenar a idade com var
+        System.out.println("Olá " + name + " sua idade é " + age); // Exibe o nome e a idade informados pelo usuário
+        System.out.printf("Olá %s sua idade é %s \n", name, age); // printf é utilizado para formatar a saída
+    }
+}
+
+
+// Primeiro código em Java
+// "new" é utilizado para instanciar objetos
+// Scanner() precisa de um argumento, que é o System.in
+// System.in é o teclado
+// \n é utilizado para pular linha
+// var é uma palavra-chave do Java que permite declarar variáveis sem especificar o tipo explicitamente
---
+++ get-started/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; //Importando a classe Scanner do pacote java.util
+import java.util.*; //Importando todas as classes do pacote java.util não recomendado, mas possível
+
+public class Main {
+    public static void main(String[] args) {
+        // Não vai parar o breakpoint
+        Scanner scanner; //Variavel scanner do tipo Scanner
+        ArrayList test; //Variavel test do tipo ArrayList
+        System.out.println("Olá");
+    }
+}
+
+//Classe sempre inicia com a letra maiúscula
+//Quando trabalhar com nomes compostos em classes, utilizar exemplo: (MinhaClasseExemplo)
+
+//Metodo é minúsculo
+//Quando trabalhar com nomes compostos em métodos, utilizar exemplo: (meuMetodoExemplo)
+
+//Não tem problema com indentação, mas é bom manter o código organizado
+//Terminar com ponto e vírgula
+
+//Ao criar pacotes, utilizar de forma contária, exemplo: (br.com.dio)
+//Duas barras para declarar comentários em uma linha
+
+//Java doc utilizado '/* */' para declarar comentários de várias linhas
+//Exemplo de comentário de uma linha é com linha e asterisco '/* */'
+/*
+ * Java é uma linguagem de programação orientada a objetos
+ * A orientação a objetos é um paradigma de programação que utiliza objetos e classes
+ */
