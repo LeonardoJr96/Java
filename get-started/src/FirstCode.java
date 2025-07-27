2ed96a42a30871d5b153201dbc493c5921798c1f
LeonardoJr96 <leo70x7000@gmail.com>
2025-07-27 17:49:28 -0300

    feat: :sparkles: Introduce constant

--- get-started/src/FirstCode.java
+++ get-started/src/FirstCode.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class FirstCode {
+
+    private final static String welcomeMensage = "Olá, informe o seu nome:"; // variavel de escopo global da classe, é uma constante
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); //Variavel scanner do tipo Scanner declarada
         //var scanner = new Scanner(System.in); // Variavel scanner do tipo Scanner declarada com var
         // "var" é uma palavra-chave do Java que permite declarar variáveis sem especificar
-        System.out.println("Olá, informe o seu nome:"); // Mensagem para o usuário
+        System.out.println(welcomeMensage); // Mensagem para o usuário
         String name = scanner.next(); // String para armazenar o nome e não é uma classe primitiva
         // var name = scanner.next(); // String para armazenar o nome com var
         System.out.println("informe sua idade:"); // Mensagem para o usuário
@@ -23,3 +25,6 @@ public class FirstCode {
 // System.in é o teclado
 // \n é utilizado para pular linha
 // var é uma palavra-chave do Java que permite declarar variáveis sem especificar o tipo explicitamente
+// private é um modificador de acesso que indica que a variável só pode ser acessada dentro da própria classe
+// final é um modificador que indica que a variável não pode ser alterada após sua inicialização
+// static é um modificador que indica que o método pertence à classe e não a uma instância específica
